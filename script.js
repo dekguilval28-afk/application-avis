1a02dbde10bed588c3a41a090e573f8b964293d9
dekguilval28-afk <dekguilval28@gmail.com>
2026-06-02 21:53:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ document.getElementById("reviewForm").addEventListener("submit", function(e) {
     this.reset();
 
 });
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAdOcIWZylYbyt6Wb4lMnX0f8yS_Tt_cnU",
+    authDomain: "avis-camping-caristes.firebaseapp.com",
+    projectId: "avis-camping-caristes",
+    storageBucket: "avis-camping-caristes.firebasestorage.app",
+    messagingSenderId: "1023687367692",
+    appId: "1:1023687367692:web:50e593a47e5018b456a229",
+    measurementId: "G-DXQDQFK3T6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
